ff7abc1384fa7764bb75b8776934b13377a0be8e
The Doans <thedoans@Thes-MacBook-Air.local>
2016-08-17 11:20:12 -0700

    Updates to move off of React

---
+++ Gruntfile.js
@@ -0,0 +1,74 @@
+module.exports= function(grunt){
+	
+	grunt.initConfig({
+
+		pkg: grunt.file.readJSON('package.json'),
+
+		sass: {
+			dist: {
+                files: {
+                    'stylesheets/<%= pkg.file_name %>.css': 'stylesheets/sass/*.scss'
+                }
+            }
+		},
+
+		'min': {
+			'dist': {
+				'options': {
+					'report': false
+				},
+				'files': [{
+					'src': ['js/echo.js','js/font.js','js/jquery.js'],
+					'dest': 'js/app.js'
+				}]
+			}
+		},
+
+		'cssmin': {
+			'dist': {
+				'options': {
+					'report': false
+				},
+				'files': [{
+					'src': ['styles/styles.css'],
+					'dest': 'styles/<%= pkg.file_name %>.min.css'
+				}]
+			}
+		},/*
+
+		watch: {
+			scripts: {
+                files: [
+                    'stylesheets/sass/*.scss'
+                ],
+                tasks: ['sass']
+            }
+		}*/
+
+		'htmlmin': {                                     // Task
+			dist: {                                      // Target
+				options: {                                 // Target options
+					removeComments: true,
+					collapseWhitespace: true,
+					minifyJS: true
+				},
+				files: {                                   // Dictionary of files
+					'index.html': 'index-original.html',     // 'destination': 'source'
+				}
+			}
+		}
+
+	});
+
+	//grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-yui-compressor');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-htmlmin');
+
+	// grunt.registerTask('w',['watch']);
+
+	//grunt.registerTask('default', ['sass','min','cssmin']);
+
+	grunt.registerTask('default', ['min', 'cssmin', 'htmlmin']);
+
+}
